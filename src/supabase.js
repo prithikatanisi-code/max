9e712c1be479abe670237f34d1b29f9d532dbc9a
prithikatanisi-code <prithikatanisi@gmail.com>
2026-09-09 18:24:01 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-)
+export const supabase =
+  supabaseUrl && supabaseKey
+    ? createClient(supabaseUrl, supabaseKey)
+    : null;
